d9c9df3ed3a8bb40387078a1346e2d0f39ae23e5
skmajumder <skmajumder.cse@gmail.com>
2023-10-09 22:51:14 +0600

    React router

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+const Home = () => {
+  return (
+    <div>
+      <PageNav />
+      WorldWise
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <PageNav />
+      <h1>Page Not Found</h1>
+    </div>
+  );
+};
+
+export default PageNotFound;
