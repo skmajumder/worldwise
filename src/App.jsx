da3e92ae16f91784f344d2e2f517019ed7c53fd9
skmajumder <skmajumder.cse@gmail.com>
2023-10-10 02:15:34 +0600

    stater file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Product from "./pages/Product";
-import Home from "./pages/Home";
+
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Homepage from "./pages/Homepage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />} />
